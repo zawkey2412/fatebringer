6a3b539d4858ebf2c1ac52fec4ea402b168b2218
zawkey2412 <24.zawkey@gmail.com>
2025-08-28 07:34:14 +0700

    module id fix

--- scripts/core/const.js
+++ scripts/core/const.js
@@ -1,21 +1,21 @@
-export const MODULE_ID = "fatebringer";
+export const MODULE_ID = "critically-fumbled";
 
 export const ROLL_CATEGORIES = {
   MELEE: "melee",
-  RANGED: "ranged", 
+  RANGED: "ranged",
   SPELL: "spell",
   SAVE: "save",
   ABILITY: "ability",
-  MANUAL: "manual"
+  MANUAL: "manual",
 };
 
 export const COLORS = {
   CRIT: { border: "#4CAF50", bg: "76,175,80" },
-  FUMBLE: { border: "#f72525ff", bg: "255,107,107" }
+  FUMBLE: { border: "#f72525ff", bg: "255,107,107" },
 };
 
 export const DELAYS = {
   DICE_ANIMATION: 2000,
   BUTTON_PROCESS: 100,
-  HOTKEY_COOLDOWN: 500
+  HOTKEY_COOLDOWN: 500,
 };
